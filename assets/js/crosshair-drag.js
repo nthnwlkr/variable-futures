ec441cda640112285a469e79bd78fdb2b4238743
Nathan Walker <nwalkerdesign@me.com>
2018-05-13 22:15:14 +1200

    Variable font axes values now respond to cursor position, x-position controls weight and y-position controls italics.

--- assets/js/crosshair-drag.js
+++ assets/js/crosshair-drag.js
@@ -1,17 +1,27 @@
 ////// DRAGGABLE CROSSHAIR HANDLE ///////
 
 //Make the DIV element draggagle:
-window.onload = function() {
-  dragElement(document.getElementById(("crosshairclicktarget")));
-} 
+dragElement(document.getElementById(("crosshairclicktarget")));
+// get window width for x-axis calculations:
+var windowWidth = window.innerWidth,
+    windowWidthPercent = windowWidth / 100,
+    windowHeight = window.innerHeight,
+    windowHeightPercent = windowHeight / 100,
+    wghtAxisMinValue = 250,
+    wghtAxisMaxValue = 900,
+    wghtAxisScaleFactor = (wghtAxisMaxValue - wghtAxisMinValue) / 100,
+    wghtCssPrefix = "'wght'",
+    italAxisMinValue = 0,  
+    italAxisMaxValue = 1,  
+    italAxisScaleFactor = (italAxisMaxValue - italAxisMinValue) / 100,
+    italCssPrefix = "'ital'",
+    textInputBox = document.getElementById("text-entry-box");
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   var textNode = document.getElementsByClassName("code-to-copy")[0];
   textNode.innerHTML = "X Position" + "<br>" + "Y Position";
   elmnt.onmousedown = dragMouseDown;
-  // add class to attach click styling:
-
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -21,6 +31,7 @@ function dragElement(elmnt) {
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
+    // add class to attach click styling:
     elmnt.classList.add("is-clicked");
   }
 
@@ -33,6 +44,14 @@ function dragElement(elmnt) {
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
+    // calculate cursor position as % of page width and height:
+    var xPercent = pos3 / windowWidthPercent;
+    var yPercent = pos4 / windowHeightPercent;
+    // calculate axis value for variable font:
+    var wghtAxisValue = xPercent * wghtAxisScaleFactor + wghtAxisMinValue; 
+    var italAxisValue = yPercent * italAxisScaleFactor + italAxisMinValue; 
+    textInputBox.setAttribute('style', 'font-variation-settings:' + wghtCssPrefix + ' ' + wghtAxisValue + ', ' + italCssPrefix + ' ' + italAxisValue );
+    console.log(wghtAxisValue);
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
@@ -45,6 +64,7 @@ function dragElement(elmnt) {
     /* stop moving when mouse button is released:*/
     document.onmouseup = null;
     document.onmousemove = null;
+    // remove class to remove click styling:
     elmnt.classList.remove("is-clicked");
   }
 }
