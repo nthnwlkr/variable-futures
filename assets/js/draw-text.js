5b24d4fded5e79fdb64eb181ae79778ff2404e59
Nathan Walker <nwalkerdesign@me.com>
2018-05-28 01:25:57 +1200

    Finished most of bounding box tool.

--- assets/js/draw-text.js
+++ assets/js/draw-text.js
@@ -71,17 +71,43 @@ function initDraw(canvas) {
 
         }
     }
-
+    function getAxes(textTarget){
+        var winHeight = $(window).height();
+        var docHeight = $(document).height();
+        var axesStringToSplit = textTarget.css('font-variation-settings');//Gets CSS object
+        var axesArray1 = axesStringToSplit.split(', ');//Splits at comma
+        var containerWidth = $(window).width();
+        var axis_wdth = containerWidth / 100;
+        var axesObject = {};//Object that the key value pairs are added to at function completion
+        for (var i = 0; i < axesArray1.length; i++) {
+            var axesArray2 = axesArray1[i].split(' ');//Splits first array at space to create 2nd array
+            axesObject[axesArray2[0].trim()] = (JSON.stringify(parseFloat(axesArray2[1])+i)).trim();//Converts 2nd array to key value pairs for final object
+            var stringifiedObject = JSON.stringify(axesObject);
+            var strippedString = stringifiedObject.replace(/["{\\}:]+/g, ' '); 
+        }
+        strippedString = strippedString.replace('wdth ', '');
+        return strippedString; 
+    }
+    var handle = {
+                h: $(".box-handle"),
+                vert: $(this).find(".box-handle.vertical"),
+                horz: document.getElementsByClassName("box-handle-horizontal")
+            }
+    $(document).on("click",".close-button",function(e){
+        var parent = this.parentNode;
+        parent.parentNode.removeChild(parent);
+    })
     canvas.onmousedown = function (e) {
         canvas.onmouseup = function(){
             element = null;
         }
-        console.log(element);
         if (element !== null) {
             element = null;
             canvas.style.cursor = "default";
             // console.log("finished.");
-        } else {
+        }else if (e.target != canvas) {
+
+        }else if (e.target = canvas) {
             // console.log("begun.");
             mouse.startX = mouse.x;
             mouse.startY = mouse.y;
@@ -89,13 +115,16 @@ function initDraw(canvas) {
             element.className = 'rectangle'
             element.style.left = mouse.x + 'px';
             element.style.top = mouse.y + 'px';
-            var handleNames = ["vertical","horizontal"];
+            var handleNames = ["vertical"];
             handleNames.forEach(function(el){
                 boxHandle = document.createElement('div');
                 boxHandle.className = "box-handle " + el;
+                // boxHandle.setAttribute("onmousedown", "clickHandle()");
                 element.appendChild(boxHandle)
             });
-
+            closeButton = document.createElement('div');
+            closeButton.className = 'close-button';
+            element.appendChild(closeButton);
             textBox = document.createElement('p'); 
             textBox.className = 'rectangle-text-box draggable';
             textBox.innerHTML = 'Type Here';
@@ -103,7 +132,67 @@ function initDraw(canvas) {
             element.appendChild(textBox);
             canvas.appendChild(element)
             canvas.style.cursor = "crosshair";
+            $(document).on("mousedown",".box-handle", function (e){
+                setMousePosition(e);
+                mouse.startX = mouse.x;
+                mouse.startY = mouse.y;
+                // console.log((xTravel - 194) + "+" + (yTravel - 45));
+                // element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
+                // element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
+                // fontSize = parseFloat(window.getComputedStyle(element).getPropertyValue('font-size'));
+                // fontSizeToAdd = parseInt((Math.abs(mouse.y - mouse.startY)) * text.fontSizeMultiplier);
+                // textBox.style.fontSize = fontSizeToAdd;
+                var mousedown = true;
+                var vertClass = "vertical",
+                    horzClass = "horizontal",
+                    parentBox = $(e.target).parent()[0],
+                    boxWidth = $(parentBox).outerWidth(),
+                    boxHeight = $(parentBox).height(),
+                    textEl = $(e.target).siblings('p'),
+                    lastWidth,
+                    lastAxis,
+                    lastVar;
+                var axisInit = getAxes(textEl);
+                var last_position = {};
+                $(textEl).addClass("focus");
+                $(document).on("mouseup", function (){
+                    mousedown = false;
+                    $(textEl).removeClass("focus");
+                    return mousedown;
+                });
+                $('#canvas').on("mousemove", function (){
+                    var direction = "positive";
+                    lastVar = axisInit;
+                    xResizeTravel = (mouse.x - mouse.startX);
+                    yResizeTravel = (mouse.y - mouse.startY);
+                    if (mousedown !== false) {
+                        if ($(e.target).hasClass(vertClass)) {
+                            axisInit = $(parentBox).outerWidth() / 1000  + xResizeTravel + 150 ;
+                            if (direction == "negative") {
+                                // axisInit = axisInit - (axisInit - 150);
+                            }
+                            lastWidth = $(parentBox).outerWidth();
+                            // $(parentBox).outerWidth(boxWidth + xResizeTravel);
+                            // var boxWidth = ($(parentBox).outerWidth()) * 150 / ($(textEl).outerWidth()) ;
+                            // console.log(axisInit);
+                            $(textEl).css('font-variation-settings', '"wdth" ' + Math.floor(axisInit) );
+                        }
+                        if ($(e.target).hasClass(horzClass)) {
+                            // $(parentBox).height(boxHeight + yResizeTravel);
+                            fontSize = parseFloat($(textEl).css("font-size"));
+                            fontSize = fontSize.replace(/["px:]+/g, ' ');
+                            fontSizeToAdd = parseInt((Math.abs(mouse.y - mouse.startY)) * text.fontSizeMultiplier);
+                            $(textEl).css("font-size", fontSizeToAdd);
+
+                        }
+                    }
+                });
+                
+            });
+            return boxHandle;
         }
         return textBox;
     }
 }
+
+    
