23d14981f8c9c71ce97ae40149fc466c42330cac
Nathan Walker <nwalkerdesign@me.com>
2018-06-10 18:32:40 +1200

    Added section 2, scrolling features and navigation.

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,19 +1,49 @@
 $(document).ready(function(){
 	var hoverColumn = $('.hover-column'),
 		letter = $('.intro-letter'),
-		columnWidth = hoverColumn.outerWidth();
-	console.log(columnWidth);
+		title = $('.title-letter'),
+		columnWidth = hoverColumn.outerWidth(),
+		scrollDistance = $(window).scrollTop(),
+		winheight = $(window).height(),
+		scrollPercent = parseInt(scrollDistance/(winheight)*100),
+		docHeight = $(document).height();
+	$(window).scroll(function() {    
+		scrollDistance = $(window).scrollTop();
+		scrollPercent = parseInt(scrollDistance/(winheight)*100);
+		if (scrollPercent < 2) {
+			$('.final-column').on('mouseout', function(f) {
+				// $('.title-letter')[0].setAttribute('style', "font-variation-settings: 'wght' 800 !important");
+			});
+		}
+		console.log(scrollPercent);
+		if (scrollPercent > 99) {
+			$('.site-nav')[0].classList.add('fixed');
+			$('.section-two-container')[0].classList.add('scrolled');
+			$('.text-container')[0].classList.remove('fixed');
+		}else {
+			$('.site-nav')[0].classList.remove('fixed');
+			$('.section-two-container')[0].classList.remove('scrolled');
+			$('.text-container')[0].classList.add('fixed');
+		}
+	});
 
-	hoverColumn.on('mouseenter', function(e){
+
+	hoverColumn.on('mouseenter', function(e){			
 		var index = hoverColumn.index(this)
 			selectedColumn = (e.target),
 			selectedLetter = letter.get(index);
-		for (i=0;i<letter.length;i++){
-			letter[i].classList.remove('show');
-			letter[i].classList.remove('wght-reset');
+		if (scrollPercent < 30){
+			for (i=0;i<letter.length;i++){
+				letter[i].classList.remove('show');
+				letter[i].classList.remove('wght-reset');
+			}
+				title[0].classList.remove('wght-reset');
+				title[0].classList.remove('show');
+			selectedLetter.classList.add('show');
+			title[0].classList.add('show');
+			title[0].classList.add('wght-reset');	
+			selectedLetter.classList.add('wght-reset');	
 		}
-		selectedLetter.classList.add('show');
-		selectedLetter.classList.add('wght-reset');	
 	    function setMousePosition(e) {
 	        var ev = e || window.event; //Moz || IE
 	        if (ev.pageX) { //Moz
@@ -34,8 +64,14 @@ $(document).ready(function(){
 	        var growFactor = columnWidth / 700,
 	        	percentageTravel = Math.round((mouse.x / columnWidth) * 100),
 	        	cssString = "'wght' " + ((percentageTravel * 7) + 100);
-		    selectedLetter.setAttribute('style', 'font-variation-settings:' + cssString  + ' !important');
+	        if (scrollPercent < 20) {
+			    selectedLetter.setAttribute('style', 'font-variation-settings:' + cssString  + ' !important');
+			    if ($(a.target).hasClass("final-column")){
+				    title[0].setAttribute('style', 'font-variation-settings:' + cssString  + ' !important');
+				}
+			}	
 		});
+		
 	});
 
 });
