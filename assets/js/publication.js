735ec51eb87cfaacc491daca0c1c474c1b50eb8f
Nathan Walker <nwalkerdesign@me.com>
2018-07-29 13:52:49 +1200

    Minor edits.

--- assets/js/publication.js
+++ assets/js/publication.js
@@ -4,13 +4,24 @@ $(document).ready(function(){
 	console.log(imgList);
 	imgList[0].classList.add("active");
 	
-	pubColumns.mouseover(function (e){
+	pubColumns.mouseenter(function (e){
 		var thisIndex = ($(e.target).index()) + 1;
 		for (i=0;i<imgList.length;i++) {
 			console.log(i);
 			imgList[i].classList.remove("active");
 		}
 		imgList[thisIndex - 1].classList.add("active");
+	});
+
+	$(document).scroll(function(){
+		pubColumns.mouseenter(function (e){
+		var thisIndex = ($(e.target).index()) + 1;
+		for (i=0;i<imgList.length;i++) {
+			console.log(i);
+			imgList[i].classList.remove("active");
+		}
+		imgList[thisIndex - 1].classList.add("active");
+	});
 	});
 
 });
