a6b560e5bb4040833fa4c401de2101f84214bf56
Nathan Walker <nwalkerdesign@me.com>
2018-07-31 22:42:49 +1200

    Publication images now fade in on scroll.

--- assets/js/publication.js
+++ assets/js/publication.js
@@ -24,4 +24,26 @@ $(document).ready(function(){
 	});
 	});
 
+	function fadeImg(e) {
+		var winHeight = $(window).height();
+	    $(e).each(function(){
+	        var thisPos = $(this).offset().top;
+			var scrollDistance = $(window).scrollTop();
+
+	        if (scrollDistance + winHeight - 200 > thisPos ) {
+	            $(this).addClass("fadeIn");
+	        }
+	    });
+	}
+
+    // if the image in the window of browser when the page is loaded, show that image
+    $(document).ready(function(){
+        fadeImg('.pub-img');
+    });
+
+    // if the image in the window of browser when scrolling the page, show that image
+    $(window).scroll(function() {
+        fadeImg('.pub-img');
+    });
+
 });
